19906e0321336155a321a86b8b1db9b87fbb7f6a
Florian Specque <f.specque@hotmail.fr>
2023-05-10 16:28:53 +0200

    Refactoring differential analysis
     - refactoring per se
     - add multiclass and timecourse tests as command line arguments (to plug)

--- src/differential_analysis.py
+++ src/differential_analysis.py
@@ -19,6 +19,11 @@ from distrib_fit_fromProteomix import \
 
 
 def diff_args():
+    timetest_methods = ('MW', 'KW', 'ranksum', 'Wcox', 'Tt', 'BrMu',
+                        'prm-scipy', 'disfit', 'none')
+    multest_methods = ('bonferroni', 'sidak', 'holm-sidak', 'holm',
+                       'simes-hochberg', 'hommel', 'fdr_bh', 'fdr_by',
+                       'fdr_tsbh', 'fdr_tsbky')
     parser = argparse.ArgumentParser(
         prog="python -m DIMet.src.differential_analysis",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -38,31 +43,43 @@ def diff_args():
 
     # Before reduction and gmean, way to replace zeros:
     parser.add_argument("--abundance_replace_zero_with",
-                        default="min", type=str, help="min | min/n | VALUE")
+                        default="min", type=zero_repl_arg,
+                        help="chose a way to replace zero-value abundances \
+(min | min/n | VALUE)")
 
     parser.add_argument("--meanEnrichOrFracContrib_replace_zero_with",
                         metavar="MEorFC_replace_zero_with",
-                        default="min", type=str,
-                        help="min | min/n | VALUE")
+                        default="min", type=zero_repl_arg,
+                        help="chose a way to replace zero-value enrichments  \
+(min | min/n | VALUE)")
 
     parser.add_argument("--isotopologueProp_replace_zero_with",
-                        default="min", type=str,
-                        help="min | min/n | VALUE")
+                        default="min", type=zero_repl_arg,
+                        help="chose a way to replace zero-value proportions of \
+isotopologues  (min | min/n | VALUE)")
 
     parser.add_argument("--isotopologueAbs_replace_zero_with",
-                        default="min", type=str,
-                        help="min | min/n | VALUE")
+                        default="min", type=zero_repl_arg,
+                        help="chose a way to replace absolute zero-values of \
+isotopologues (min | min/n | VALUE)")
 
     parser.add_argument(
-        "--multitest_correction", default='fdr_bh',
+        "--multitest_correction", default='fdr_bh', choices=multest_methods,
         help="see : https://www.statsmodels.org/dev/generated/\
-        statsmodels.stats.multitest.multipletests.html")
+statsmodels.stats.multitest.multipletests.html")
 
     parser.add_argument(
         "--qualityDistanceOverSpan", default=-0.5, type=float,
         help="By metabolite, for samples (x, y) the distance is calculated,\
         and span is max(x U y) - min(x U y).  A 'distance/span' inferior\
         to this value excludes the metabolite from testing (pvalue=NaN).")
+    
+    parser.add_argument('--multiclass_test', choices=('KW', 'none'),
+                        type=str, default='none',
+                        help='chose a test for more than 2 classes')
+    parser.add_argument('--time_course', choices=timetest_methods,
+                        type=str, default='none',
+                        help='chose a test for 2-by-2 comparison of time-points')
 
     # by default include all the types of measurements:
 
@@ -78,36 +95,34 @@ def diff_args():
     return parser
 
 
-def validate_zero_repl_arg(zero_repl_arg: str) -> None:
-    isvalid = False
-    if zero_repl_arg == "min":
-        isvalid = True
-    elif zero_repl_arg.startswith("min/"):
-        try:
-            float(str(zero_repl_arg.split("/")[1]))
-            isvalid = True
-        except ValueError:
-            pass
+def zero_repl_arg(zero_repl_arg: str) -> None:
+    zero_repl_arg = zero_repl_arg.lower()
+    err_msg = 'replace_zero_with argument is not correctly formatted'
+    if zero_repl_arg.startswith("min"):
+        if zero_repl_arg == "min":
+            n=int(1)
+        else:
+            try:
+                n=float(str(zero_repl_arg.split("/")[1]))
+            except:
+                raise ValueError(err_msg)
+        def foo(x,n):
+            return(min(x)/n)
     else:
         try:
-            float(args.zero_repl_arg)
-            isvalid = True
-        except ValueError:
-            pass
-
-    assert isvalid, "Your value for zero replacement is not valid"
+            n=float(str(zero_repl_arg))
+        except:
+            raise ValueError(err_msg)
+        def foo(x,n):
+            return(n)
+    return {'repZero': foo, 'n': n}
 
 
 def arg_repl_zero2value(argum_zero_rep: str, df: pd.DataFrame) -> float:
-    if argum_zero_rep.startswith("min"):
-        try:
-            divisor_min = float(argum_zero_rep.split("/")[1])
-        except IndexError:
-            divisor_min = 1
-        value_out = df[df > 0].min().min() / divisor_min
-    else:
-        value_out = float(args.zero_repl_arg)
-    return value_out
+    repZero = argum_zero_rep['repZero']
+    n = argum_zero_rep['n']
+    replacement = repZero(df[df > 0].apply(repZero, n=1), n=n)
+    return replacement
 
 
 def flag_has_replicates(ratiosdf: pd.DataFrame):
@@ -846,12 +861,36 @@ def multiclass_timecourse_diff2groups(measurements, meta_co,
         run_differential_steps(measurements, meta_co, out_file_elems,
                                confidic, whichtest, args)
 
-
-def wrapper_for_abund(clean_tables_path, table_prefix,
-                      metadatadf, confidic, args) -> None:
-    out_diff_abun = out_path + "results/differential_analysis/abundance/"
-    fg.detect_and_create_dir(out_diff_abun)
-    whichtest = confidic['statistical_test']['abundances']
+modes_specs={
+    'abund':{
+        'dir': 'abundance',
+        'test_key': 'abundances'
+    },
+    'mefc': {
+        'dir': 'meanE_fracContr',
+        'test_key': 'meanE_or_fracContrib'
+    },
+    'isoabsol': {
+        'dir': 'isotopol_abs',
+        'test_key': 'isotopologue_abs'
+    },
+    'isoprop': {
+        'dir': 'isotopol_prop',
+        'test_key': 'isotopologue_prop'
+    }
+}
+    
+def perform_tests(mode: str, clean_tables_path, table_prefix,
+                  metadatadf, confidic, args) -> None:
+    mode=mode.lower()
+    if not mode in modes_specs.keys():
+        raise ValueError(f'unknown mode "{mode}", \
+possible values are {list(modes_specs)}')
+    
+    out_diff = out_path + "results/differential_analysis/" + \
+               modes_specs[mode]['dir']
+    fg.detect_and_create_dir(out_diff)
+    whichtest = confidic['statistical_test'][modes_specs[mode]['test_key']]
     suffix = confidic['suffix']
     compartments = metadatadf['short_comp'].unique().tolist()
     # dynamically open the file based on prefix, compartment and suffix:
@@ -863,88 +902,8 @@ def wrapper_for_abund(clean_tables_path, table_prefix,
             args.abundance_replace_zero_with, measurements)
         measurements = measurements.replace(to_replace=0,
                                             value=val_instead_zero)
-        out_file_elems = {'odir': out_diff_abun, 'prefix': table_prefix,
-                          'co': co, 'suffix': suffix}
-
-        multiclass_timecourse_diff2groups(measurements, meta_co,
-                                          out_file_elems,
-                                          confidic, whichtest, args)
-
-
-def wrapper_for_mefc(clean_tables_path, table_prefix,
-                     metadatadf, confidic, args) -> None:
-    out_diff = out_path + "results/differential_analysis/meanE_fracContr/"
-    fg.detect_and_create_dir(out_diff)
-    whichtest = confidic['statistical_test']['meanE_or_fracContrib']
-    suffix = confidic['suffix']
-    compartments = metadatadf['short_comp'].unique().tolist()
-    # dynamically open the file based on prefix, compartment and suffix:
-    for co in compartments:
-        meta_co = metadatadf.loc[metadatadf['short_comp'] == co, :]
-        fn = f'{clean_tables_path}{table_prefix}--{co}--{suffix}.tsv'
-        measurements = pd.read_csv(fn, sep='\t', header=0, index_col=0)
-        val_instead_zero = arg_repl_zero2value(
-            args.meanEnrichOrFracContrib_replace_zero_with,
-            measurements)
-        measurements = measurements.replace(to_replace=0,
-                                            value=val_instead_zero)
         out_file_elems = {'odir': out_diff, 'prefix': table_prefix,
                           'co': co, 'suffix': suffix}
-        #run_differential_steps(measurements, meta_co, out_file_elems, confidic, whichtest, args)
-        multiclass_timecourse_diff2groups(measurements, meta_co,
-                                          out_file_elems,
-                                          confidic, whichtest, args)
-
-
-
-def wrapper_for_isoAbsol(clean_tables_path, table_prefix,
-                         metadatadf, confidic, args) -> None:
-    out_diff = out_path + "results/differential_analysis/isotopol_abs/"
-    fg.detect_and_create_dir(out_diff)
-    whichtest = confidic['statistical_test']['isotopologue_abs']
-    suffix = confidic['suffix']
-    compartments = metadatadf['short_comp'].unique().tolist()
-    # dynamically open the file based on prefix, compartment and suffix:
-    for co in compartments:
-        meta_co = metadatadf.loc[metadatadf['short_comp'] == co, :]
-        fn = f'{clean_tables_path}{table_prefix}--{co}--{suffix}.tsv'
-        measurements = pd.read_csv(fn, sep='\t', header=0, index_col=0)
-        val_instead_zero = arg_repl_zero2value(
-            args.isotopologueAbs_replace_zero_with,
-            measurements)
-        measurements = measurements.replace(to_replace=0,
-                                            value=val_instead_zero)
-        out_file_elems = {'odir': out_diff, 'prefix': table_prefix,
-                          'co': co, 'suffix': suffix}
-
-        #run_differential_steps(measurements, meta_co,  out_file_elems,  confidic, whichtest, args)
-        multiclass_timecourse_diff2groups(measurements, meta_co,
-                                          out_file_elems,
-                                          confidic, whichtest, args)
-
-
-def wrapper_for_isoProp(clean_tables_path, table_prefix,
-                        metadatadf, confidic, args) -> None:
-    out_diff = out_path + "results/differential_analysis/isotopol_prop/"
-    fg.detect_and_create_dir(out_diff)
-    whichtest = confidic['statistical_test']['isotopologue_prop']
-    suffix = confidic['suffix']
-    compartments = metadatadf['short_comp'].unique().tolist()
-    # dynamically open the file based on prefix, compartment and suffix:
-    for co in compartments:
-        meta_co = metadatadf.loc[metadatadf['short_comp'] == co, :]
-        fn = f'{clean_tables_path}{table_prefix}--{co}--{suffix}.tsv'
-        measurements = pd.read_csv(fn, sep='\t', header=0, index_col=0)
-
-        val_instead_zero = arg_repl_zero2value(
-            args.isotopologueProp_replace_zero_with,
-            measurements)
-        measurements = measurements.replace(to_replace=0,
-                                            value=val_instead_zero)
-        out_file_elems = {'odir': out_diff, 'prefix': table_prefix,
-                          'co': co, 'suffix': suffix}
-
-        #run_differential_steps(measurements, meta_co, out_file_elems, confidic, whichtest, args)
 
         multiclass_timecourse_diff2groups(measurements, meta_co,
                                           out_file_elems,
@@ -970,35 +929,35 @@ if __name__ == "__main__":
     # 1- abund
     if args.abundances:
         print("processing abundances")
-        validate_zero_repl_arg(args.abundance_replace_zero_with)
+        mode="abund"
         abund_tab_prefix = confidic['name_abundance']
-        wrapper_for_abund(clean_tables_path, abund_tab_prefix,
-                          metadatadf, confidic, args)
+        perform_tests(mode, clean_tables_path, abund_tab_prefix,
+                      metadatadf, confidic, args)
 
     # 2- ME or FC
     if args.meanEnrich_or_fracContrib:
         print("processing mean enrichment or fractional contributions")
-        validate_zero_repl_arg(args.meanEnrichOrFracContrib_replace_zero_with)
+        mode="mefc"
         fraccon_tab_prefix = confidic['name_meanE_or_fracContrib']
-        wrapper_for_mefc(clean_tables_path, fraccon_tab_prefix,
+        perform_tests(mode, clean_tables_path, fraccon_tab_prefix,
                          metadatadf, confidic, args)
 
     # 3- isotopologues
     if args.isotopologues:
+        mode="isoabsol"
         isos_abs_tab_prefix = confidic['name_isotopologue_abs']
         isos_prop_tab_prefix = confidic['name_isotopologue_prop']
         if (isos_abs_tab_prefix is not np.nan) and \
                 (isos_abs_tab_prefix != "None") and \
                 (isos_abs_tab_prefix is not None):
             print("processing absolute isotopologues")
-            validate_zero_repl_arg(args.isotopologueAbs_replace_zero_with)
-            wrapper_for_isoAbsol(clean_tables_path, isos_abs_tab_prefix,
-                                 metadatadf, confidic, args)
+            perform_tests(mode, clean_tables_path, isos_abs_tab_prefix,
+                          metadatadf, confidic, args)
         else:
             print("processing isotopologues (values given as proportions)")
-            validate_zero_repl_arg(args.isotopologueProp_replace_zero_with)
-            wrapper_for_isoProp(clean_tables_path, isos_prop_tab_prefix,
-                                metadatadf, confidic, args)
+            mode="isoprop"
+            perform_tests(mode, clean_tables_path, isos_prop_tab_prefix,
+                          metadatadf, confidic, args)
 
     print("end")
 # #END
